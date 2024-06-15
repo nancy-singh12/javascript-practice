04da22cba6325f8dbadb64bfa93d7f7e52ffde77
nancy-singh12 <153458396+nancy-singh12@users.noreply.github.com>
2024-06-15 20:07:36 +0000

    conversion Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,41 @@
+let score = "33"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abcc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "nnancy" => true
+
+let marks =  33
+let grades = String(marks)
+console.log(grades);
+console.log(typeof grades);
+
+// *********** Operations ***********//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Nancy"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(+true);
+console.log(+"");
+
