aed5b0bc8bdb903fbea59ecbd000d287e4a02f52
nancy-singh12 <153458396+nancy-singh12@users.noreply.github.com>
2024-06-15 19:17:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer version
+
+// alert (3 + 3) we are using nodejs, not browser
+
+console.log(2 + 5)//code readability should be high
+
+let name = "nancy"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => unique
+// object
+
+console.log(typeof(age));
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
