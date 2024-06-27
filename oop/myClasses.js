3e48beb157f89ed274f9ef114d9a84706c4bb387
nancy-singh12 <153458396+nancy-singh12@users.noreply.github.com>
2024-06-27 17:34:29 +0000

    classes in js

---
+++ oop/myClasses.js
@@ -0,0 +1,41 @@
+class User {
+    constructor(username, email, password){ //whenever the object will be initialised from the  class by using new keyword the constructor will  be called 
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+
+}
+
+const chai = new User("chai", "chai@gmail.com", "123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
