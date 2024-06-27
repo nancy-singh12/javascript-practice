00f6c87529a42b267edf5292a3cb39e7e7df4648
nancy-singh12 <153458396+nancy-singh12@users.noreply.github.com>
2024-06-27 17:38:00 +0000

    get and set in function

---
+++ oop/properties_get_set.js
@@ -0,0 +1,26 @@
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', { //define property has get and set property inside it
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+
+console.log(chai.email);
